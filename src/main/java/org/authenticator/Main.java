16791e3219383388d349299e2150e120fdfb487c
Nolwenn Cadic <nolwenn.cadic@sonarsource.com>
2023-10-04 22:38:54 +0200

    Implement google authenticator app

--- src/main/java/org/authenticator/Main.java
+++ src/main/java/org/authenticator/Main.java
@@ -1,7 +1,28 @@
 package org.authenticator;
 
+import com.google.zxing.WriterException;
+import java.io.IOException;
+import java.util.Scanner;
+import org.apache.log4j.Logger;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  private static final Logger logger = Logger.getLogger(Main.class);
+
+  public static void main(String[] args) throws IOException, WriterException {
+    String secretKey = "QDWSM3OYBPGTEVSPB5FKVDM3CSNCWHVK";
+    String email = "test@gmail.com";
+    String companyName = "Awesome Company";
+    String barCodeUrl = Utils.getGoogleAuthenticatorBarCode(secretKey, email, companyName);
+    Utils.createQRCode(barCodeUrl, "QRCode.png", 400, 400);
+
+
+    logger.info("Please enter 2fA code here -> ");
+    Scanner scanner = new Scanner(System.in);
+    String code = scanner.nextLine();
+    if (code.equals(Utils.getTOTPCode(secretKey))) {
+      logger.info("Logged in successfully");
+    } else {
+      logger.info("Invalid 2FA Code");
+    }
   }
 }
